b502b76ddc52f70b47a94beeec690462025774ec
Raman Dutt <happy07.dutt@gmail.com>
2017-06-21 16:53:35 +0530

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,14 @@
+import subprocess as s
+import time
+
+def countdown(n):
+	while n >0:
+		n=n-1
+		time.sleep(1)
+		if n==0:
+			s.call(['notify-send', '20-20-20', 'Woah man! Take a rest for 20 sec!!'])
+
+countdown(60)
+
+
+
