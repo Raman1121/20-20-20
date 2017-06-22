5861cf2ec8a3ac6cb77b9c28a8c52dff57fb2883
Raman Dutt <happy07.dutt@gmail.com>
2017-06-22 13:43:10 +0530

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,12 +1,15 @@
 import subprocess as s
 import time
+import os
 
 def countdown(n):
 	while n >0:
 		n=n-1
 		time.sleep(1)
 		if n==0:
-			s.call(['notify-send', '20-20-20', 'Woah man! Take a rest for 20 sec!!'])
+			img = os.path.abspath('20-20-20.jpg')
+			
+			s.Popen(['notify-send', '-i', img, '20-20-20', 'Woah man! Take a rest for 20 sec!!'])
 
 countdown(1200)
 
